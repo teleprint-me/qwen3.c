05d5dc5eb9fa03b46e8314a8cc2eb2ef3f895d9d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-07-09 03:27:16 -0400

    qwen3(feat): Integrate abstracted interfaces

--- qwen3/__main__.py
+++ qwen3/__main__.py
@@ -3,22 +3,37 @@
 @brief Converts a Qwen3ForCausalLM architecture to a custom binary format.
 """
 
-import argparse
-from qwen3.convert import load_model, build_tokenizer, build_prompts, model_export
+from argparse import ArgumentParser, Namespace
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from qwen3.weights import model_params, model_load, model_write
+from qwen3.tokenizer import template_write, tokenizer_vocab, tokenizer_write
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
     parser.add_argument("output_file", type=str, help="The output file path.")
     parser.add_argument("input_dir", type=str, help="The input dir path.")
+    parser.add_argument(
+        "-g",
+        "--group-size",
+        type=int,
+        default=64,
+        help="Number of values per quantization group.",
+    )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
 
-    # Convert Qwen3 from pytorch to Q8 binary file
-    model = load_model(args.input_dir)
-    tokenizer = build_tokenizer(model, args.input_dir, args.output_file)
-    build_prompts(tokenizer, args.output_file)
-    model_export(model, args.output_file)
+    # Export the tokenizer model
+    tokenizer = AutoTokenizer.from_pretrained(args.input_dir)
+    template_write(tokenizer, args.output_file)
+    vocab = tokenizer_vocab(tokenizer)
+    tokenizer_write(vocab, args.output_file)
+
+    # Export the model weights
+    state = AutoModelForCausalLM.from_pretrained(args.input_dir).state_dict()
+    params = model_params(args.input_dir)
+    model = model_load(params, state)
+    model_write(model, args.output_file, args.group_size)
