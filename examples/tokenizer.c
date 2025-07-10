28df55985fcdc4d71bc38e41a534d539a513c659
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-07-10 03:00:50 -0400

    examples(feat): Add a simple driver for smoke testing the tokenizer model

---
+++ examples/tokenizer.c
@@ -0,0 +1,30 @@
+/**
+ * @file examples/tokenizer.c
+ */
+
+#include "tokenizer.h"
+
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#define VOCAB_SIZE 151669
+
+int main(int argc, const char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "%s <tokenizer-model> <text>\n", *argv);
+        return 1;
+    }
+    const char* model_path = argv[1];
+
+    Tokenizer* tokenizer = tokenizer_create(model_path, VOCAB_SIZE, 0);
+
+    for (int i = 2; i < argc; i++) {
+        int id = tokenizer_token_to_id(tokenizer, argv[i]);
+        char* token = tokenizer_id_to_token(tokenizer, id);
+        printf("[Tokenizer] id=%d, token=%s\n", id, token);
+    }
+
+    tokenizer_free(tokenizer);
+    return 0;
+}
